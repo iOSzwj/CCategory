de1ead641fc14382d064f5e12c247717d6027c73
hare27 <hare27@foxmail.com>
2019-05-22 18:48:19 +0800

    5.22 本地验证通过

---
+++ podspecEditing/Pods/Target Support Files/Pods-podspecEditing/Pods-podspecEditing-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_podspecEditing : NSObject
+@end
+@implementation PodsDummy_Pods_podspecEditing
+@end
---
+++ podspecEditing/podspecEditing/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  podspecEditing
+//
+//  Created by ioszwj on 2019/5/22.
+//  Copyright © 2019 hare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
