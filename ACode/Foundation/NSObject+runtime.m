8d65a222701e94be57aaf640ee11813b8def7f74
hare27 <hare27@foxmail.com>
2019-05-23 15:27:59 +0800

    5.23 修改了某个bug

--- ACode/Foundation/NSObject+runtime.m
+++ ACode/Foundation/NSObject+runtime.m
@@ -9,12 +9,12 @@
 #import "NSObject+runtime.h"
 
 @implementation NSObject (runtime)
-
+    
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-
+    
 #pragma mark - 返回值为id类型
-
+    
 - (id)executeSel:(SEL)sel{
     if ([self respondsToSelector:sel]) {
         return [self performSelector:sel];
@@ -35,7 +35,7 @@
     SEL sel = NSSelectorFromString(selName);
     return [self executeSel:sel];
 }
-
+    
 - (id)executeSel:(SEL)sel obj:(id)obj{
     if ([self respondsToSelector:sel]) {
         return [self performSelector:sel withObject:obj];
@@ -56,7 +56,7 @@
     SEL sel = NSSelectorFromString(selName);
     return [self executeSel:sel obj:obj];
 }
-
+    
 - (id)executeSel:(SEL)sel obj:(id)obj1 obj:(id)obj2{
     if ([self respondsToSelector:sel]) {
         return [self performSelector:sel withObject:obj1 withObject:obj2];
@@ -77,72 +77,71 @@
     SEL sel = NSSelectorFromString(selName);
     return [self executeSel:sel obj:obj1 obj:obj2];
 }
-
+    
 #pragma mark - 返回值为bool类型
-
+    
 - (BOOL)bExecuteSel:(SEL)sel{
     if ([self respondsToSelector:sel]) {
-        return [self performSelector:sel];
+        return (BOOL)[self performSelector:sel];
     }
-    return nil;
+    return NO;
 }
 + (BOOL)bExecuteSel:(SEL)sel{
     if ([self respondsToSelector:sel]) {
-        return [self performSelector:sel];
+        return (BOOL)[self performSelector:sel];
     }
-    return nil;
+    return NO;
 }
 - (BOOL)bExecuteSelName:(NSString *)selName{
     SEL sel = NSSelectorFromString(selName);
-    return [self executeSel:sel];
+    return [self bExecuteSel:sel];
 }
 + (BOOL)bExecuteSelName:(NSString *)selName{
     SEL sel = NSSelectorFromString(selName);
-    return [self executeSel:sel];
+    return [self bExecuteSel:sel];
 }
-
+    
 - (BOOL)bExecuteSel:(SEL)sel obj:(id)obj{
     if ([self respondsToSelector:sel]) {
-        return [self performSelector:sel withObject:obj];
+        return (BOOL)[self performSelector:sel withObject:obj];
     }
-    return nil;
+    return NO;
 }
 + (BOOL)bExecuteSel:(SEL)sel obj:(id)obj{
     if ([self respondsToSelector:sel]) {
-        return [self performSelector:sel withObject:obj];
+        return (BOOL)[self performSelector:sel withObject:obj];
     }
-    return nil;
+    return NO;
 }
 - (BOOL)bExecuteSelName:(NSString *)selName obj:(id)obj{
     SEL sel = NSSelectorFromString(selName);
-    return [self executeSel:sel obj:obj];
+    return [self bExecuteSel:sel obj:obj];
 }
 + (BOOL)bExecuteSelName:(NSString *)selName obj:(id)obj{
     SEL sel = NSSelectorFromString(selName);
-    return [self executeSel:sel obj:obj];
+    return [self bExecuteSel:sel obj:obj];
 }
-
+    
 - (BOOL)bExecuteSel:(SEL)sel obj:(id)obj1 obj:(id)obj2{
     if ([self respondsToSelector:sel]) {
-        return [self performSelector:sel withObject:obj1 withObject:obj2];
+        return (BOOL)[self performSelector:sel withObject:obj1 withObject:obj2];
     }
-    return nil;
+    return NO;
 }
 + (BOOL)bExecuteSel:(SEL)sel obj:(id)obj1 obj:(id)obj2{
     if ([self respondsToSelector:sel]) {
-        return [self performSelector:sel withObject:obj1 withObject:obj2];
+        return (BOOL)[self performSelector:sel withObject:obj1 withObject:obj2];
     }
-    return nil;
+    return NO;
 }
 - (BOOL)bExecuteSelName:(NSString *)selName obj:(id)obj1 obj:(id)obj2{
     SEL sel = NSSelectorFromString(selName);
-    return [self executeSel:sel obj:obj1 obj:obj2];
+    return [self bExecuteSel:sel obj:obj1 obj:obj2];
 }
 + (BOOL)bExecuteSelName:(NSString *)selName obj:(id)obj1 obj:(id)obj2{
     SEL sel = NSSelectorFromString(selName);
-    return [self executeSel:sel obj:obj1 obj:obj2];
+    return [self bExecuteSel:sel obj:obj1 obj:obj2];
 }
-
+    
 #pragma clang diagnostic pop
-
 @end
