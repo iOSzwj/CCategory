26202707f9a4bef45e423e4342c92aed5251141f
王园园 <yuanyuan6244@163.com>
2019-11-15 17:01:50 +0800

    11.15 performSelector返回值判断返回对象类型。

--- ACode/Foundation/NSObject+runtime.m
+++ ACode/Foundation/NSObject+runtime.m
@@ -7,7 +7,7 @@
 //
 
 #import "NSObject+runtime.h"
-
+#import <objc/runtime.h>
 @implementation NSObject (runtime)
     
 #pragma clang diagnostic push
@@ -17,20 +17,113 @@
     
 - (id)executeSel:(SEL)sel{
     if ([self respondsToSelector:sel]) {
+        //方法返回值类型
+        char retType[512] = {};
+        Method method = class_getInstanceMethod([self class], sel);
+        method_getReturnType(method, retType, 512);
+        if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"Q"])
+        {
+            unsigned long long num = (unsigned long long)[self performSelector:sel];
+            return [NSNumber numberWithUnsignedLongLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"q"])
+        {
+            long long num = (long long)[self performSelector:sel];
+            return [NSNumber numberWithLongLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"I"])
+        {
+            unsigned int num = (unsigned int)[self performSelector:sel];
+            return [NSNumber numberWithUnsignedInt:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"i"])
+        {
+           int num = (int)[self performSelector:sel];
+            return [NSNumber numberWithInt:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"S"])
+        {
+            unsigned short num = (unsigned short)[self performSelector:sel];
+            return [NSNumber numberWithUnsignedShort:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"s"])
+        {
+            short num = (short)[self performSelector:sel];
+            return [NSNumber numberWithShort:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"L"])
+        {
+            unsigned long num = (unsigned long)[self performSelector:sel];
+            return [NSNumber numberWithUnsignedLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"l"])
+        {
+            long num = (long)[self performSelector:sel];
+            return [NSNumber numberWithLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"C"])
+        {
+            unsigned char num = (unsigned char)[self performSelector:sel];
+            return [NSNumber numberWithUnsignedChar:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"c"])
+        {
+            char num = (char)[self performSelector:sel];
+            return [NSNumber numberWithChar:num];
+        }
         return [self performSelector:sel];
     }
     return nil;
 }
+
 + (id)executeSel:(SEL)sel{
     if ([self respondsToSelector:sel]) {
+        //方法返回值类型
+        char retType[512] = {};
+        Method method = class_getClassMethod([self class], sel);
+        method_getReturnType(method, retType, 512);
+        if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"Q"])
+        {
+            unsigned long long num = (unsigned long long)[self performSelector:sel];
+            return [NSNumber numberWithUnsignedLongLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"q"])
+        {
+            long long num = (long long)[self performSelector:sel];
+            return [NSNumber numberWithLongLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"I"])
+        {
+            unsigned int num = (unsigned int)[self performSelector:sel];
+            return [NSNumber numberWithUnsignedInt:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"i"])
+        {
+           int num = (int)[self performSelector:sel];
+            return [NSNumber numberWithInt:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"S"])
+        {
+            unsigned short num = (unsigned short)[self performSelector:sel];
+            return [NSNumber numberWithUnsignedShort:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"s"])
+        {
+            short num = (short)[self performSelector:sel];
+            return [NSNumber numberWithShort:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"L"])
+        {
+            unsigned long num = (unsigned long)[self performSelector:sel];
+            return [NSNumber numberWithUnsignedLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"l"])
+        {
+            long num = (long)[self performSelector:sel];
+            return [NSNumber numberWithLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"C"])
+        {
+            unsigned char num = (unsigned char)[self performSelector:sel];
+            return [NSNumber numberWithUnsignedChar:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"c"])
+        {
+            char num = (char)[self performSelector:sel];
+            return [NSNumber numberWithChar:num];
+        }
         return [self performSelector:sel];
     }
     return nil;
 }
+
 - (id)executeSelName:(NSString *)selName{
     SEL sel = NSSelectorFromString(selName);
     return [self executeSel:sel];
 }
+
 + (id)executeSelName:(NSString *)selName{
     SEL sel = NSSelectorFromString(selName);
     return [self executeSel:sel];
@@ -38,12 +131,102 @@
     
 - (id)executeSel:(SEL)sel obj:(id)obj{
     if ([self respondsToSelector:sel]) {
+        //方法返回值类型
+        char retType[512] = {};
+        Method method = class_getInstanceMethod([self class], sel);
+        method_getReturnType(method, retType, 512);
+        if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"Q"])
+        {
+            unsigned long long num = (unsigned long long)[self performSelector:sel withObject:obj];
+            return [NSNumber numberWithUnsignedLongLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"q"])
+        {
+            long long num = (long long)[self performSelector:sel withObject:obj];
+            return [NSNumber numberWithLongLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"I"])
+        {
+            unsigned int num = (unsigned int)[self performSelector:sel withObject:obj];
+            return [NSNumber numberWithUnsignedInt:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"i"])
+        {
+           int num = (int)[self performSelector:sel withObject:obj];
+            return [NSNumber numberWithInt:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"S"])
+        {
+            unsigned short num = (unsigned short)[self performSelector:sel withObject:obj];
+            return [NSNumber numberWithUnsignedShort:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"s"])
+        {
+            short num = (short)[self performSelector:sel withObject:obj];
+            return [NSNumber numberWithShort:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"L"])
+        {
+            unsigned long num = (unsigned long)[self performSelector:sel withObject:obj];
+            return [NSNumber numberWithUnsignedLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"l"])
+        {
+            long num = (long)[self performSelector:sel withObject:obj];
+            return [NSNumber numberWithLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"C"])
+        {
+            unsigned char num = (unsigned char)[self performSelector:sel withObject:obj];
+            return [NSNumber numberWithUnsignedChar:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"c"])
+        {
+            char num = (char)[self performSelector:sel withObject:obj];
+            return [NSNumber numberWithChar:num];
+        }
         return [self performSelector:sel withObject:obj];
     }
     return nil;
 }
 + (id)executeSel:(SEL)sel obj:(id)obj{
     if ([self respondsToSelector:sel]) {
+        //方法返回值类型
+        char retType[512] = {};
+        Method method = class_getClassMethod([self class], sel);
+        method_getReturnType(method, retType, 512);
+        if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"Q"])
+        {
+            unsigned long long num = (unsigned long long)[self performSelector:sel withObject:obj];
+            return [NSNumber numberWithUnsignedLongLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"q"])
+        {
+            long long num = (long long)[self performSelector:sel withObject:obj];
+            return [NSNumber numberWithLongLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"I"])
+        {
+            unsigned int num = (unsigned int)[self performSelector:sel withObject:obj];
+            return [NSNumber numberWithUnsignedInt:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"i"])
+        {
+           int num = (int)[self performSelector:sel withObject:obj];
+            return [NSNumber numberWithInt:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"S"])
+        {
+            unsigned short num = (unsigned short)[self performSelector:sel withObject:obj];
+            return [NSNumber numberWithUnsignedShort:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"s"])
+        {
+            short num = (short)[self performSelector:sel withObject:obj];
+            return [NSNumber numberWithShort:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"L"])
+        {
+            unsigned long num = (unsigned long)[self performSelector:sel withObject:obj];
+            return [NSNumber numberWithUnsignedLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"l"])
+        {
+            long num = (long)[self performSelector:sel withObject:obj];
+            return [NSNumber numberWithLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"C"])
+        {
+            unsigned char num = (unsigned char)[self performSelector:sel withObject:obj];
+            return [NSNumber numberWithUnsignedChar:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"c"])
+        {
+            char num = (char)[self performSelector:sel withObject:obj];
+            return [NSNumber numberWithChar:num];
+        }
         return [self performSelector:sel withObject:obj];
     }
     return nil;
@@ -59,12 +242,102 @@
     
 - (id)executeSel:(SEL)sel obj:(id)obj1 obj:(id)obj2{
     if ([self respondsToSelector:sel]) {
+        //方法返回值类型
+        char retType[512] = {};
+        Method method = class_getInstanceMethod([self class], sel);
+        method_getReturnType(method, retType, 512);
+        if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"Q"])
+        {
+            unsigned long long num = (unsigned long long)[self performSelector:sel withObject:obj1 withObject:obj2];
+            return [NSNumber numberWithUnsignedLongLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"q"])
+        {
+            long long num = (long long)[self performSelector:sel withObject:obj1 withObject:obj2];
+            return [NSNumber numberWithLongLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"I"])
+        {
+            unsigned int num = (unsigned int)[self performSelector:sel withObject:obj1 withObject:obj2];
+            return [NSNumber numberWithUnsignedInt:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"i"])
+        {
+           int num = (int)[self performSelector:sel withObject:obj1 withObject:obj2];
+            return [NSNumber numberWithInt:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"S"])
+        {
+            unsigned short num = (unsigned short)[self performSelector:sel withObject:obj1 withObject:obj2];
+            return [NSNumber numberWithUnsignedShort:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"s"])
+        {
+            short num = (short)[self performSelector:sel withObject:obj1 withObject:obj2];
+            return [NSNumber numberWithShort:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"L"])
+        {
+            unsigned long num = (unsigned long)[self performSelector:sel withObject:obj1 withObject:obj2];
+            return [NSNumber numberWithUnsignedLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"l"])
+        {
+            long num = (long)[self performSelector:sel withObject:obj1 withObject:obj2];
+            return [NSNumber numberWithLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"C"])
+        {
+            unsigned char num = (unsigned char)[self performSelector:sel withObject:obj1 withObject:obj2];
+            return [NSNumber numberWithUnsignedChar:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"c"])
+        {
+            char num = (char)[self performSelector:sel withObject:obj1 withObject:obj2];
+            return [NSNumber numberWithChar:num];
+        }
         return [self performSelector:sel withObject:obj1 withObject:obj2];
     }
     return nil;
 }
 + (id)executeSel:(SEL)sel obj:(id)obj1 obj:(id)obj2{
     if ([self respondsToSelector:sel]) {
+        //方法返回值类型
+        char retType[512] = {};
+        Method method = class_getClassMethod([self class], sel);
+        method_getReturnType(method, retType, 512);
+        if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"Q"])
+        {
+            unsigned long long num = (unsigned long long)[self performSelector:sel withObject:obj1 withObject:obj2];
+            return [NSNumber numberWithUnsignedLongLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"q"])
+        {
+            long long num = (long long)[self performSelector:sel withObject:obj1 withObject:obj2];
+            return [NSNumber numberWithLongLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"I"])
+        {
+            unsigned int num = (unsigned int)[self performSelector:sel withObject:obj1 withObject:obj2];
+            return [NSNumber numberWithUnsignedInt:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"i"])
+        {
+           int num = (int)[self performSelector:sel withObject:obj1 withObject:obj2];
+            return [NSNumber numberWithInt:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"S"])
+        {
+            unsigned short num = (unsigned short)[self performSelector:sel withObject:obj1 withObject:obj2];
+            return [NSNumber numberWithUnsignedShort:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"s"])
+        {
+            short num = (short)[self performSelector:sel withObject:obj1 withObject:obj2];
+            return [NSNumber numberWithShort:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"L"])
+        {
+            unsigned long num = (unsigned long)[self performSelector:sel withObject:obj1 withObject:obj2];
+            return [NSNumber numberWithUnsignedLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"l"])
+        {
+            long num = (long)[self performSelector:sel withObject:obj1 withObject:obj2];
+            return [NSNumber numberWithLong:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"C"])
+        {
+            unsigned char num = (unsigned char)[self performSelector:sel withObject:obj1 withObject:obj2];
+            return [NSNumber numberWithUnsignedChar:num];
+        }else if( [[NSString stringWithFormat:@"%s",retType] isEqualToString:@"c"])
+        {
+            char num = (char)[self performSelector:sel withObject:obj1 withObject:obj2];
+            return [NSNumber numberWithChar:num];
+        }
         return [self performSelector:sel withObject:obj1 withObject:obj2];
     }
     return nil;
--- CCategory.podspec
+++ CCategory.podspec
@@ -4,7 +4,7 @@
 Pod::Spec.new do |spec|
   
   spec.name         = "CCategory"
-  spec.version      = "0.1.1"
+  spec.version      = "0.1.2"
   spec.summary      = "中国共产党万岁"
   
   spec.description  = <<-DESC
